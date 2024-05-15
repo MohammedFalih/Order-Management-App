30745dddfcc29c831abdd369015f7bd32f852cf4
Mohammed Falih <yunusfalih123@gmail.com>
2024-05-15 22:28:55 +0530

    analytics-final-commit

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -1,10 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { columnData, data, Revenue, Sales, Average, Profits, SalesTax } from './analytics.data';
 
 @Component({
   selector: 'app-analytics',
   templateUrl: './analytics.component.html',
   styleUrl: './analytics.component.css'
 })
-export class AnalyticsComponent {
+export class AnalyticsComponent implements OnInit {
 
+  public yAxis?: {};
+  gridOptions?: any;
+  columnData = columnData;
+  data = data;
+  revenue = Revenue;
+  sales = Sales;
+  average = Average;
+  profit = Profits;
+  salesTax = SalesTax;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.yAxis = {
+      ticks: {
+        number: 5, // Tip: round max data value
+        format: 'd'
+      }
+    };
+    this.gridOptions = {
+      columns: this.COLUMNS,
+      dataset: this.data,
+      selectable: 'single',
+      idProperty: 'productId',
+      stretchColumn: 'all',
+      editable: true,
+      isList: true,
+      filterable: true
+    };
+  }
+
+  COLUMNS: SohoDataGridColumn[] = [
+    {
+      id: 'month',
+      name: 'Monthly',
+      field: 'month',
+      sortable: false
+    },
+
+    {
+      id: 'revenue',
+      name: 'Revenue',
+      field: 'value',
+      sortable: false,
+    },
+    {
+      id: 'sales',
+      name: 'Sales',
+      field: 'sales',
+      sortable: false
+    },
+    {
+      id: 'avgticket',
+      name: 'Average Ticket Size',
+      field: 'average',
+      sortable: false
+    },
+  ]
 }
---
+++ src/app/analytics/analytics.data.ts
@@ -0,0 +1,118 @@
+
+
+export const columnData: any[] = [{
+    data: [{
+        name: 'January',
+        value: 254.15,
+        tooltip: '{{name}} {{value}}',
+        sales: 12,
+        month: 'January',
+        average: '$25.40'
+    }, {
+        name: 'February',
+        value: 240.10,
+        tooltip: '{{name}} {{value}}',
+        sales: 15,
+        month: 'February',
+        average: '$24.01'
+    }, {
+        name: 'March',
+        value: 515.30,
+        tooltip: '{{name}} {{value}}',
+        sales: 13,
+        month: 'March',
+        average: '$51.53'
+    }, {
+        name: 'April',
+        value: 423.40,
+        tooltip: '{{name}} {{value}}',
+        sales: 8,
+        month: 'April',
+        average: '$42.34'
+    }, {
+        name: 'May',
+        value: 360.50,
+        tooltip: '{{name}} {{value}}',
+        sales: 23,
+        month: 'May',
+        average: '$36.05'
+    }, {
+        name: 'June',
+        value: 530.50,
+        tooltip: '{{name}} {{value}}',
+        sales: 20,
+        month: 'June',
+        average: '$53.05'
+    }]
+}];
+
+export const data: any[] = [
+    {
+        name: 'January',
+        value: 254.15,
+        tooltip: '{{name}} {{value}}',
+        sales: 12,
+        month: 'January',
+        average: '$25.40'
+    }, {
+        name: 'February',
+        value: 240.10,
+        tooltip: '{{name}} {{value}}',
+        sales: 15,
+        month: 'February',
+        average: '$24.01'
+    }, {
+        name: 'March',
+        value: 515.30,
+        tooltip: '{{name}} {{value}}',
+        sales: 13,
+        month: 'March',
+        average: '$51.53'
+    }, {
+        name: 'April',
+        value: 423.40,
+        tooltip: '{{name}} {{value}}',
+        sales: 8,
+        month: 'April',
+        average: '$42.34'
+    }, {
+        name: 'May',
+        value: 360.50,
+        tooltip: '{{name}} {{value}}',
+        sales: 23,
+        month: 'May',
+        average: '$36.05'
+    }, {
+        name: 'June',
+        value: 530.50,
+        tooltip: '{{name}} {{value}}',
+        sales: 20,
+        month: 'June',
+        average: '$53.05'
+    }
+];
+
+export const Revenue = [{
+    data: [254.15, 240.10, 515.30, 423.40, 360.50, 530.50],
+    name: 'Revenue'
+}];
+
+export const Sales = [{
+    data: [12, 15, 13, 8, 23, 20],
+    name: 'Sales'
+}];
+
+export const Average = [{
+    data: [25.40, 24.01, 51.53, 42.34, 36.05, 53.05],
+    name: 'Avg Ticket'
+}];
+
+export const Profits = [{
+    data: [42.34, 25.40, 53.05, 51.53, 36.05, 24.01],
+    name: 'Profits'
+}];
+
+export const SalesTax = [{
+    data: [22.34, 45.40, 13.05, 61.53, 56.05, 64.01],
+    name: 'Sales Tax'
+}];
